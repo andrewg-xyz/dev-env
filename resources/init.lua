49499335fbed46d9395394b0da55c7756c49d4e6
andrew-xyz <andrew@defenseunicorns.com>
2023-11-27 09:21:19 -0700

    brew: dagger, hammerspoon: config update

--- resources/init.lua
+++ resources/init.lua
@@ -1,24 +1,113 @@
 hs.window.animationDuration = 0
 units = {
-  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  upright50     = { x = 0.50, y = 0.00, w = 0.50, h = 0.50 },
-  botright50    = { x = 0.50, y = 0.50, w = 0.50, h = 0.50 },
-  upleft50      = { x = 0.00, y = 0.00, w = 0.50, h = 0.50 },
-  botleft50     = { x = 0.00, y = 0.50, w = 0.50, h = 0.50 },
-  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+    right50 = {
+        x = 0.50,
+        y = 0.00,
+        w = 0.50,
+        h = 1.00
+    },
+    left50 = {
+        x = 0.00,
+        y = 0.00,
+        w = 0.50,
+        h = 1.00
+    },
+    top50 = {
+        x = 0.00,
+        y = 0.00,
+        w = 1.00,
+        h = 0.50
+    },
+    bot50 = {
+        x = 0.00,
+        y = 0.50,
+        w = 1.00,
+        h = 0.50
+    },
+    upright50 = {
+        x = 0.50,
+        y = 0.00,
+        w = 0.50,
+        h = 0.50
+    },
+    botright50 = {
+        x = 0.50,
+        y = 0.50,
+        w = 0.50,
+        h = 0.50
+    },
+    upleft50 = {
+        x = 0.00,
+        y = 0.00,
+        w = 0.50,
+        h = 0.50
+    },
+    botleft50 = {
+        x = 0.00,
+        y = 0.50,
+        w = 0.50,
+        h = 0.50
+    },
+    maximum = {
+        x = 0.00,
+        y = 0.00,
+        w = 1.00,
+        h = 1.00
+    },
+    left33 = {
+        x = 0.00,
+        y = 0.00,
+        w = 0.33,
+        h = 1.00
+    },
+    mid33 = {
+        x = 0.33,
+        y = 0.00,
+        w = 0.34,
+        h = 1.00
+    }, -- Use 0.34 width to cover rounding issues
+    right33 = {
+        x = 0.67,
+        y = 0.00,
+        w = 0.33,
+        h = 1.00
+    }
 }
 
-
 mash = {"cmd", "alt", "ctrl"}
-hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
-hs.hotkey.bind(mash, 'left', function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
-hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
-hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
-hs.hotkey.bind(mash, '1', function() hs.window.focusedWindow():move(units.upleft50,   nil, true) end)
-hs.hotkey.bind(mash, '2', function() hs.window.focusedWindow():move(units.upright50,  nil, true) end)
-hs.hotkey.bind(mash, '3', function() hs.window.focusedWindow():move(units.botleft50,  nil, true) end)
-hs.hotkey.bind(mash, "4", function() hs.window.focusedWindow():move(units.botright50, nil, true) end)
-hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
+hs.hotkey.bind(mash, 'right', function()
+    hs.window.focusedWindow():move(units.right50, nil, true)
+end)
+hs.hotkey.bind(mash, 'left', function()
+    hs.window.focusedWindow():move(units.left50, nil, true)
+end)
+hs.hotkey.bind(mash, 'up', function()
+    hs.window.focusedWindow():move(units.top50, nil, true)
+end)
+hs.hotkey.bind(mash, 'down', function()
+    hs.window.focusedWindow():move(units.bot50, nil, true)
+end)
+hs.hotkey.bind(mash, '1', function()
+    hs.window.focusedWindow():move(units.upleft50, nil, true)
+end)
+hs.hotkey.bind(mash, '2', function()
+    hs.window.focusedWindow():move(units.upright50, nil, true)
+end)
+hs.hotkey.bind(mash, '3', function()
+    hs.window.focusedWindow():move(units.botleft50, nil, true)
+end)
+hs.hotkey.bind(mash, "4", function()
+    hs.window.focusedWindow():move(units.botright50, nil, true)
+end)
+hs.hotkey.bind(mash, '5', function()
+    hs.window.focusedWindow():move(units.left33, nil, true)
+end)
+hs.hotkey.bind(mash, '6', function()
+    hs.window.focusedWindow():move(units.mid33, nil, true)
+end)
+hs.hotkey.bind(mash, '7', function()
+    hs.window.focusedWindow():move(units.right33, nil, true)
+end)
+hs.hotkey.bind(mash, 'm', function()
+    hs.window.focusedWindow():move(units.maximum, nil, true)
+end)
